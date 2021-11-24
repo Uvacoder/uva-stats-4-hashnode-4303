edcb45edb20b908d39525f20c2d8aec34d5871e2
avneesh0612 <avneeshagarwal0612@gmail.com>
2021-11-24 21:20:19 +0530

    added somethings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNProgress />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
---
+++ types/PostType.ts
@@ -0,0 +1,11 @@
+export type PostType = {
+  _id: string;
+  title: string;
+  content: string;
+  brief: string;
+  totalReactions: string;
+  replyCount: string;
+  responseCount: string;
+  coverImage: string;
+  slug: string;
+};
