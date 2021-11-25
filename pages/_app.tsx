6fd75ea561c2ec69cbdcaa9180f8ac7670b1bfaf
avneesh0612 <avneeshagarwal0612@gmail.com>
2021-11-25 09:21:15 +0530

    fix styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import "tailwindcss/tailwind.css";
+import "../styles/globals.css";
 import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
